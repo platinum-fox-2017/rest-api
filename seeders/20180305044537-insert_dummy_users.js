436291bd143b5cebcaf5de6f0ae8f8f451398416
Gustaf Pahlevi <gustafpahlevi@gmail.com>
2018-03-05 13:02:56 +0700

    Release 3

---
+++ seeders/20180305044537-insert_dummy_users.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'johndoe@mail.com',
+        password: '123',
+        first_name: 'John',
+        last_name: 'Doe',
+        phone: 0812345612,
+        user_level: 0812345612,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
