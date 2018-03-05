da8b9a4b9459a5af31e5ef853b99ed2640a8602a
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-03-05 13:07:34 +0700

    CRUD User with seperate controller and routes

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,24 +1,63 @@
-const signUp = (req, res) => {
+const {
+    User
+} = require('../models')
 
+const signUp = (req, res) => {
+    const input = {
+        email: req.body.email,
+        password: req.body.password
+    }
+    User.create(input).then(() => {
+        res.send('User signup')
+    })
 }
 const signIn = (req, res) => {
 
 }
 const getAllUsers = (req, res) => {
-
+    User.findAll().then((datas) => {
+        res.send(datas)
+    })
 }
 const getAUser = (req, res) => {
-
+    const id = req.params.id
+    User.findById(id).then((data) => {
+        res.send(data)
+    })
 }
 const createUser = (req, res) => {
-
+    const input = {
+        email: req.body.email,
+        password: req.body.password
+    }
+    User.create(input).then(() => {
+        res.send('User signup')
+    })
 }
 const deleteUser = (req, res) => {
-
+    const id = req.params.id
+    User.destroy({
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.send('User Deleted')
+    })
 }
 
 const updateUser = (req, res) => {
-
+    const id = req.params.id
+    const input = {
+        email: req.body.email,
+        password: req.body.password
+    }
+    User.update(input, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.send('data updated')
+    })
 }
 
 module.exports = {
